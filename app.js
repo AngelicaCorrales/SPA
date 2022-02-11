fe8054b98bc9f5dfd2c18c8df664180878c86fb3
AngelicaCorrales <angelicacorrales2002@gmail.com>
2022-02-11 17:36:56 -0500

    Rounting part added

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ const routes={
     , '/register': Regsiter
 };
 
+const router= async()=>{
+    const content=null || document.getElementById('page_container');
+
+    let request=Utils.parseRequesrURL();
+
+    let parseURL=(reuest.resource?'/'+request.resource:'/')+(request.id?'/:id':'')+(request.verb?'/'+request.verb:'');
+
+    let page= routes[parseURL]? routes[parseURL]:Error404
+    content.innerHTML=await page.render();
+    await page.after_render();
+}
+
+window.addEventListener('hashchange',router);
+
+window.addEventListener('load', router);
+
+
+
